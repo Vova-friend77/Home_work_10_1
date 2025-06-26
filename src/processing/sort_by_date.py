660bdfb613ec4dc0dc1158d411b1fc418ba86a21
Vladimir Metelev <vladimir-metelev@yandex.ru>
2025-06-26 22:28:57 +1000

    add file sort_by_date.py

---
+++ src/processing/sort_by_date.py
@@ -0,0 +1,7 @@
+
+
+def sort_by_date(my_list: list, reverse=False) -> list:
+    '''Функция сортирует список словарей по дате'''  # noqa
+    sorted_list = sorted(my_list, key=lambda x: x['date'], reverse=reverse)
+    return sorted_list
+
