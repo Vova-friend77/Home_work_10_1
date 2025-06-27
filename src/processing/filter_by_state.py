5b73a89c8f8f1f62dd234dcbe596d26ed878998e
Vladimir Metelev <vladimir-metelev@yandex.ru>
2025-06-27 21:09:28 +1000

    add file .gitignore

--- src/processing/filter_by_state.py
+++ src/processing/filter_by_state.py
@@ -4,9 +4,8 @@ def filter_by_state(list_of_dict: list, state='EXECUTED') -> list:
        соответствует указанному значению, по умолчанию
        state = EXECUTED"""                              # noqa
 
-
     filtered_list = []
     for dic in list_of_dict:
-         if dic.get('state') == state:
-             filtered_list.append(dic)
+        if dic.get('state') == state:
+            filtered_list.append(dic)
     return filtered_list
